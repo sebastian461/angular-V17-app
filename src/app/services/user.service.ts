c50912e1e39643e45938393b2e58d630ba9af7ae
sebastian461 <sebastiano-461@hotmail.com>
2024-04-19 17:52:02 -0500

    Uso de servicios sin modulos

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { User, UserResponse } from '../interfaces/req-response.interfaces';
+import { HttpClient } from '@angular/common/http';
+import { delay } from 'rxjs';
+
+interface State {
+  users: User[];
+  loading: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private http = inject(HttpClient);
+
+  //* Usar '#' es similar a usar 'private'
+  #state = signal<State>({
+    loading: true,
+    users: [],
+  });
+
+  public users = computed(() => this.#state().users);
+  public loading = computed(() => this.#state().loading);
+
+  constructor() {
+    this.http
+      .get<UserResponse>('https://reqres.in/api/users')
+      .pipe(delay(1000))
+      .subscribe((res) => {
+        this.#state.set({
+          loading: false,
+          users: res.data,
+        });
+      });
+  }
+}
