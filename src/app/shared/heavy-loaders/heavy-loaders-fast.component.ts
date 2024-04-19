0ef6e4af490d1dec5c7e9b7bd9d13e8d2573727b
sebastian461 <sebastiano-461@hotmail.com>
2024-04-18 20:05:17 -0500

    Estructura de la aplicación

---
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  template: ` <h1>Hola mundo</h1> `,
+  styles: ``,
+})
+export class HeavyLoadersFastComponent {}
