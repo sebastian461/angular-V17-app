969c83a72eb953e46b85305a2261ef669df6a795
sebastian461 <sebastiano-461@hotmail.com>
2024-04-19 12:55:49 -0500

    Defer view
    Carga de vistas con el uso de @defer, cuando los componentes son muy pesados

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '../../../shared/heavy-loaders/heavy-loaders-slow.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [HeavyLoadersSlowComponent],
   templateUrl: './defer-views.component.html',
   styles: ``,
 })
--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
+  selector: 'app-heavy-loader-slow',
   standalone: true,
-  imports: [],
-  template: ` <h1>Hola mundo</h1> `,
+  imports: [CommonModule],
+  template: `
+    <section [ngClass]="['w-full h-[600px]', cssClass]">
+      HeavyLoaderSlow
+    </section>
+  `,
   styles: ``,
 })
-export class HeavyLoadersSlowComponent {}
+export class HeavyLoadersSlowComponent {
+  @Input({ required: true }) cssClass!: string;
+  constructor() {
+    const start = Date.now();
+
+    while (Date.now() - start < 3000) {}
+
+    console.log('Cargado');
+  }
+}
