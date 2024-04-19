ddec1fd871ad68566c8a303dfa2f0a5c8e5bac09
sebastian461 <sebastiano-461@hotmail.com>
2024-04-18 21:17:31 -0500

    Control flow - @for

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -11,6 +11,8 @@ type Grade = 'A' | 'B' | 'F';
 export default class ControlFlowComponent {
   public showContent = signal(false);
   public grade = signal<Grade>('A');
+  public frameworks = signal(['Angular', 'Vue', 'Sveltr', 'Qwik', 'React']);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
     this.showContent.update((value) => !value);
